aee59b27bbf7811071f22be16923a5ae56f9bc69
arundevx <arundev2003x@gmail.com>
2026-06-25 00:04:31 +0530

    chore: restrict cpus and worker threads

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   trailingSlash: true,
+  experimental: {
+    cpus: 1,
+    workerThreads: false,
+  },
   images: {
     unoptimized: true,
   },
